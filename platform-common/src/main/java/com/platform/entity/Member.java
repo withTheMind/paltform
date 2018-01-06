1fe156f8569a9185db5dbb8d3abc2ead6c1ad23b
phone15082037343 <15082037343@163.com>
2018-01-06 16:06:43 +0800

    登录账号+密码

--- platform-common/src/main/java/com/platform/entity/Member.java
+++ platform-common/src/main/java/com/platform/entity/Member.java
@@ -1,5 +1,36 @@
 package com.platform.entity;
 
-public class Member {
+import java.io.Serializable;
 
+/**
+ * 会员
+ * @author Administrator
+ *
+ */
+public class Member implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private int memberId;
+	private String phone;//手机号 登录账号
+	private String password;//密码
+	
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
 }
