d0da6ff1d5f85ae44fcd987450eb2b76b5a7ae7d
phone15082037343 <15082037343@163.com>
2018-01-07 09:58:42 +0800

    memberController

---
+++ paltform-member/src/main/java/com/paltform/member/controller/MemberController.java
@@ -0,0 +1,22 @@
+package com.paltform.member.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Scope("prototype")
+@RequestMapping("/member")
+public class MemberController {
+	
+	
+	/**
+	 * 个人中心
+	 * @return
+	 */
+	@RequestMapping("/centerPage")
+	public String centerPage(){
+		return "";
+	}
+	
+}
