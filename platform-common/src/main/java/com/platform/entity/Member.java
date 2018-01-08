4f08fe46d1749818260cc9e0a6a60d02521434ad
phone15082037343 <15082037343@163.com>
2018-01-08 18:42:14 +0800

    增加昵称 邮箱

--- platform-common/src/main/java/com/platform/entity/Member.java
+++ platform-common/src/main/java/com/platform/entity/Member.java
@@ -13,6 +13,8 @@ public class Member implements Serializable{
 	private int memberId;
 	private String phone;//手机号 登录账号
 	private String password;//密码
+	private String nickName;//昵称
+	private String email;//邮箱
 	
 	public int getMemberId() {
 		return memberId;
@@ -32,5 +34,18 @@ public class Member implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
 	
 }
